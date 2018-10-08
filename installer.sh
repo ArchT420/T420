dd964acdd2d219d22df02a46f8c147ded57030ec
ArchT420 <43963869+ArchT420@users.noreply.github.com>
2018-10-08 21:48:50 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,14 +12,14 @@
 # Set the mirrorlist from https://www.archlinux.org/mirrorlist/
 # and rank 5 best mirrors, while commenting out the rest.
 
-MIRRORLIST_URL="https://www.archlinux.org/mirrorlist/?country=FI&country=LV&country=NO&country=PL&country=SE&protocol=https&use_mirror_status=on"
+#MIRRORLIST_URL="https://www.archlinux.org/mirrorlist/?country=FI&country=LV&country=NO&country=PL&country=SE&protocol=https&use_mirror_status=on"
 
-pacman -Sy --noconfirm pacman-contrib
+#pacman -Sy --noconfirm pacman-contrib
 
-echo "Updating & Ranking the mirror list in: /etc/pacman.d/mirrorlist"
-curl -s "$MIRRORLIST_URL" | \
-    sed -e 's/^#Server/Server/' -e '/^#/d' | \
-    rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+#echo "Updating & Ranking the mirror list in: /etc/pacman.d/mirrorlist"
+#curl -s "$MIRRORLIST_URL" | \
+#    sed -e 's/^#Server/Server/' -e '/^#/d' | \
+#    rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
 
 ### Get infomation from user ###
@@ -37,3 +37,9 @@ clear
 password2=$(dialog --stdout --passwordbox "Retype default user password" 0 0) || exit 1
 clear
 [[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
+
+gdisk /dev/sda
+x
+z
+y
+y
