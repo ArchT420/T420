af5f3713f631d3c843bcb05259633da5e16ed7b2
ArchT420 <43963869+ArchT420@users.noreply.github.com>
2018-10-09 03:42:11 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -75,7 +75,7 @@ mount "${part_boot}" /mnt/boot
 mount "${part_home}" /mnt/home
 
 ## Install the base Arch system
-pacstrap -i /mnt base base-devel --noconfirm
+pacstrap -i /mnt base base-devel
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
 ##### arch-chroot #####
