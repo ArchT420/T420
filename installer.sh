f95eae49480383c9a6b9f68a1fc21eca89595f7f
ArchT420 <43963869+ArchT420@users.noreply.github.com>
2018-10-10 08:32:51 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script can be run by executing the following:
 # curl -sL https://git.io/fxcQv | bash
+part_root="$(ls ${device}* | grep -E "^${device}p?3$")"	#   /	partition3 created by sgdisk
 echo -e 'title\tArch Linux\nlinux\t/vmlinuz-linux\ninitrd\t/initrd\t/initramfs-linux.img\noptions\troot=PARTUUID=$(blkid -s PARTUUID -o value "$part_root") rw' > /mnt/boot/loader/entries/arch.conf
 
