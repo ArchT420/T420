1e435373d4c43cd9d0ba6efcd5f8b6abe9de7fce
ArchT420 <43963869+ArchT420@users.noreply.github.com>
2018-10-10 22:08:05 +0300

    Create installerv3.sh

---
+++ installerv3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# curl -sL https://git.io/fxcQv | bash
+
+logger(){
+	echo -e "${CYAN}Output & Error logging has now been enabled.:${WHITE} ~/.stdout.log stderr.log${NC}\n"
+	exec 1> >(tee "stdout.log")
+	exec 2> >(tee "stderr.log")
+	sleep 5
+}
+
+logger
