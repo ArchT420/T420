7d5d0130bcd863e84a286533acbc3c7c7fd5bd34
ArchT420 <43963869+ArchT420@users.noreply.github.com>
2018-10-10 09:17:06 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ part_swap="$(ls ${device}* | grep -E "^${device}p?2$")"	# /swap partition2 creat
 part_root="$(ls ${device}* | grep -E "^${device}p?3$")"	#   /	partition3 created by sgdisk
 part_home="$(ls ${device}* | grep -E "^${device}p?4$")"	# /home partition4 created by sgdisk
 
-echo -e "title\tArch Linux\nlinux\t/vmlinuz-linux\ninitrd\t/initrd\t/initramfs-linux.img\noptions\troot=PARTUUID=$(blkid -s PARTUUID -o value "${part_root}") rw" > /mnt/boot/loader/entries/arch.conf
+echo -e "title\tArch Linux\nlinux\t/vmlinuz-linux\ninitrd\t/initrd\t/initramfs-linux.img\noptions\troot=PARTUUID=$(blkid -s PARTUUID -o value ${part_root}) rw" > /mnt/boot/loader/entries/arch.conf
 
 echo "done1"
 echo "hyep"
